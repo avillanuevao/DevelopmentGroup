dcd09f28e8c0664e92c150783a2e62635c64cbac
Adrian <adrian_fg_@hotmail.com>
2024-01-19 10:16:53 +0100

    Model as a dynamic library

--- DDS_Bank_HMI/model/AllFunds.hpp
+++ DDS_Bank_HMI/model/AllFunds.hpp
@@ -16,6 +16,7 @@ class AllFunds : public utils::designPattern::SignalPublisher<model::signal::Mon
         AllFunds();
 
         void increaseAmount(model::FundType fundType, int amount);
+        void decereaseAmount(model::FundType fundType, int amount);
         int getAmount(model::FundType fundType) const;
         void setAmount(model::FundType fundType, int newAmount);
 
